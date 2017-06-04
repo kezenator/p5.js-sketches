ab684de52c978109e29bb724db207e2f986e5983
kezenator <github@kezenator.com>
2017-06-04 11:27:48 +1000

    Completed first working version. Added sliders to control image

--- web/sketches/cycloid-spiral/cycloid-spiral.js
+++ web/sketches/cycloid-spiral/cycloid-spiral.js
@@ -42,24 +42,53 @@ function cycloidSpiralFunction(p)
 
     var points = [];
     var cycloidOmega = 0.05;
-    var cycloidRadius = 0.1456123;
     var cycloidAngle = 0;
     var spiralAngle = 0;
+    
+    var showGuidesCheckbox;
+    var invertSpiralDirCheckbox;
+    var numPointsSlider;
+    var cycloidRadiusSlider;
+    var spiralFactorSlider;
 
     p.setup = function()
     {
         p.createCanvas(500, 500);
         p.background(0);
+        
+        var clearBtn = p.createButton("Clear");
+        clearBtn.mousePressed(function() {points = [];});
+        
+        showGuidesCheckbox = p.createCheckbox("Show guides", true);
+        invertSpiralDirCheckbox = p.createCheckbox("Invert spiral direction", false);
+
+        p.createP("Number of points:");
+        numPointsSlider = p.createSlider(100, 20000, 2000, 100);
+    
+        p.createP("Cycloid Size:");
+        cycloidRadiusSlider = p.createSlider(0.05, 0.95, 0.25, 0.0005);
+    
+        p.createP("Spiral Size:");
+        spiralFactorSlider = p.createSlider(0.995, 1.0, 0.9995, 0.00005);
     }
 
     p.draw = function()
     {
+        // Get the current values from the sliders
+        
+        var cycloidRadius = cycloidRadiusSlider.value();
+        console.log(cycloidRadius);
+        
         // First, update the cycloid and spiral angles
         // now that we've drawn a new frame.
 
         cycloidAngle = (cycloidAngle + cycloidOmega) % p.TWO_PI;
 
         var spiralOmega = (cycloidOmega * cycloidRadius);
+        
+        if (invertSpiralDirCheckbox.checked())
+            spiralOmega = -spiralOmega;
+        
         spiralAngle = (spiralAngle + spiralOmega) % p.TWO_PI;
 
         // Calculate the current position of
@@ -72,24 +101,32 @@ function cycloidSpiralFunction(p)
 
         var cycloidPoint = p5.Vector.add(cycloidCenter, cycloidOffset);
 
-        // Draw the outside circle
-
+        // Start with a fresh background
+        
         p.background(0);
 
-        p.stroke(255);
-        p.noFill();
-        p.ellipseMode(p.CORNER);
-        p.ellipse(toScreenX(-1), toScreenY(1),
-                  toScreenX(1), toScreenY(-1));
+        // Draw the outside circle
+        
+        if (showGuidesCheckbox.checked())
+        {
+            p.stroke(255);
+            p.noFill();
+            p.ellipseMode(p.CORNER);
+            p.ellipse(toScreenX(-1), toScreenY(1),
+                      toScreenX(1), toScreenY(-1));
+        }
 
         // Draw the cycloid
 
-        p.ellipseMode(p.RADIUS);
-        p.ellipse(toScreenX(cycloidCenter.x), toScreenY(cycloidCenter.y),
-                  scaleScreenX(cycloidRadius), scaleScreenY(cycloidRadius));
- 
-        p.line(toScreenX(cycloidCenter.x), toScreenY(cycloidCenter.y),
-               toScreenX(cycloidPoint.x), toScreenY(cycloidPoint.y));
+        if (showGuidesCheckbox.checked())
+        {
+            p.ellipseMode(p.RADIUS);
+            p.ellipse(toScreenX(cycloidCenter.x), toScreenY(cycloidCenter.y),
+                      scaleScreenX(cycloidRadius), scaleScreenY(cycloidRadius));
+
+            p.line(toScreenX(cycloidCenter.x), toScreenY(cycloidCenter.y),
+                   toScreenX(cycloidPoint.x), toScreenY(cycloidPoint.y));
+        }
 
         // Conver the cycloid point to a radius and angle and add
         // it as a new point into the points array. Also clear down any remaining points.
@@ -98,7 +135,7 @@ function cycloidSpiralFunction(p)
 
         points.unshift(newPoint);
 
-        while (points.length > 2000)
+        while (points.length > numPointsSlider.value())
         {
             points.pop();
         }
@@ -112,8 +149,10 @@ function cycloidSpiralFunction(p)
         for (i = 0; i < points.length; ++i)
         {
             var point = points[i];
+            
+            var radiusFactor = Math.pow(spiralFactorSlider.value(), i);
 
-            var location = p.createVector(point.radius, 0);
+            var location = p.createVector(point.radius * radiusFactor, 0);
             location.rotate(point.angleOffset);
             location.rotate(curAngle);
 
@@ -121,6 +160,7 @@ function cycloidSpiralFunction(p)
 
             if (i > 0)
             {
+                p.stroke(255, 255, 255, 255 - (255 * i / points.length));
                 p.line(
                     toScreenX(lastLocation.x), toScreenY(lastLocation.y),
                     toScreenX(location.x), toScreenY(location.y));
